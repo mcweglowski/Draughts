a1f0523f73e0df855af030efb47c60a5e091c87e
Marcin Weglowski <marcin@escali.no>
2019-01-08 11:17:12 +0100

    Create Unit test for DraughtsGameTwoRowsInitializer

---
+++ DraughtsGame/DraughtsGameTwoRowsInitializer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraughtsGame
+{
+    public class DraughtsGameTwoRowsInitializer
+    {
+        private ICheesboard cheesboard;
+
+        public DraughtsGameTwoRowsInitializer(ICheesboard cheesboard)
+        {
+            this.cheesboard = cheesboard;
+        }
+
+        public void InitializeNewGame()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DraughtsGameUnitTests/DraughtsGameTwoRowsInitializerTest.cs
@@ -0,0 +1,16 @@
+﻿using DraughtsGame;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DraughtsGameUnitTests
+{
+    public class DraughtsGameTwoRowsInitializerTest
+    {
+        public void TestIfTwoRowsGameInitialized()
+        {
+            ICheesboard cheesboard = new Cheesboard();
+
+            DraughtsGameTwoRowsInitializer draughtsGameTwoRowsInitializer = new DraughtsGameTwoRowsInitializer(cheesboard);
+            draughtsGameTwoRowsInitializer.InitializeNewGame();
+        }
+    }
+}
