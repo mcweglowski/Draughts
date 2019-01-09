0b5900d518c75b74cfda702d3105f9aee533d987
Marcin Weglowski <marcin@escali.no>
2019-01-09 08:17:39 +0100

    Setup white pawns,tests. Setup white pawns logic.

--- DraughtsGame/DraughtsGameTwoRowsInitializer.cs
+++ DraughtsGame/DraughtsGameTwoRowsInitializer.cs
@@ -9,6 +9,7 @@ namespace DraughtsGame
     public class DraughtsGameTwoRowsInitializer
     {
         private ICheesboard cheesboard;
+        private const int RowsCount = 2;
 
         public DraughtsGameTwoRowsInitializer(ICheesboard cheesboard)
         {
@@ -17,7 +18,33 @@ namespace DraughtsGame
 
         public void InitializeNewGame()
         {
-            throw new NotImplementedException();
+            SetupWhitePawns();
+            SetupRedPawns();
+        }
+
+        private void SetupRedPawns()
+        {
+        }
+
+        private void SetupWhitePawns()
+        {
+            for (CheesboardRow Row = CheesboardRow.One; (int)Row < RowsCount; Row++)
+            {
+                for (CheesboardColumn Column = GetFirstBlackFieldForRow(Row); (int)Column < cheesboard.GetCheesboardWidth(); Column = Column + 2)
+                {
+                    cheesboard.SetFieldState(Row, Column, CheesboardField.WhitePawn);
+                }
+            }
+        }
+
+        private CheesboardColumn GetFirstBlackFieldForRow(CheesboardRow Row)
+        {
+            if (0 == (int)Row % 2)
+            {
+                return CheesboardColumn.A;
+            }
+
+            return CheesboardColumn.B;
         }
     }
 }
--- DraughtsGameUnitTests/DraughtsGameTwoRowsInitializerTest.cs
+++ DraughtsGameUnitTests/DraughtsGameTwoRowsInitializerTest.cs
@@ -25,11 +25,11 @@ namespace DraughtsGameUnitTests
             DraughtsGameTwoRowsInitializer draughtsGameTwoRowsInitializer = new DraughtsGameTwoRowsInitializer(cheesboard);
             draughtsGameTwoRowsInitializer.InitializeNewGame();
 
-            Assert.AreEqual(8, cheesboard.WhitePawnsCount, "There should be 8 white pawns.");
+            Assert.AreEqual(8, cheesboard.RedPawnsCount, "There should be 8 red pawns.");
         }
 
         [TestMethod()]
-        public void TestIfTwoRowsGameInitializedPlacedWhitePawnOnA1()
+        public void TestIfTwoRowsGameInitializeNotDefinedOnA1()
         {
             CheesboardStub cheesboard = new CheesboardStub();
 
@@ -37,6 +37,40 @@ namespace DraughtsGameUnitTests
             draughtsGameTwoRowsInitializer.InitializeNewGame();
 
             Assert.AreEqual(CheesboardField.WhitePawn, cheesboard.A1, "A1 field pawn should be White.");
+        }
+
+        [TestMethod()]
+        public void TestIfTwoRowsGameInitializedPlacedWhitePawnOnA2()
+        {
+            CheesboardStub cheesboard = new CheesboardStub();
+
+            DraughtsGameTwoRowsInitializer draughtsGameTwoRowsInitializer = new DraughtsGameTwoRowsInitializer(cheesboard);
+            draughtsGameTwoRowsInitializer.InitializeNewGame();
+
+            Assert.AreEqual(CheesboardField.NotDefined, cheesboard.A2, "A2 field pawn should be NotDefined.");
+        }
+
+        [TestMethod()]
+        public void TestIfTwoRowsGameInitializedPlacedNotDefinedOnB1()
+        {
+            CheesboardStub cheesboard = new CheesboardStub();
+
+            DraughtsGameTwoRowsInitializer draughtsGameTwoRowsInitializer = new DraughtsGameTwoRowsInitializer(cheesboard);
+            draughtsGameTwoRowsInitializer.InitializeNewGame();
+
+            Assert.AreEqual(CheesboardField.NotDefined, cheesboard.B1, "B1 field pawn should be NotDefined.");
+        }
+
+
+        [TestMethod()]
+        public void TestIfTwoRowsGameInitializedPlacedWhitePawnOnB2()
+        {
+            CheesboardStub cheesboard = new CheesboardStub();
+
+            DraughtsGameTwoRowsInitializer draughtsGameTwoRowsInitializer = new DraughtsGameTwoRowsInitializer(cheesboard);
+            draughtsGameTwoRowsInitializer.InitializeNewGame();
+
+            Assert.AreEqual(CheesboardField.WhitePawn, cheesboard.B2, "B2 field pawn should be White.");
         }
 
         [TestMethod()]
@@ -57,16 +91,19 @@ namespace DraughtsGameUnitTests
         public int RedPawnsCount = 0;
 
         public CheesboardField A1;
+        public CheesboardField A2;
+        public CheesboardField B1;
+        public CheesboardField B2;
         public CheesboardField H8;
 
         public int GetCheesboardHeight()
         {
-            throw new System.NotImplementedException();
+            return 8;
         }
 
         public int GetCheesboardWidth()
         {
-            throw new System.NotImplementedException();
+            return 8;
         }
 
         public CheesboardField GetFieldState(CheesboardRow row, CheesboardColumn column)
@@ -88,6 +125,21 @@ namespace DraughtsGameUnitTests
             if (CheesboardRow.One == row && CheesboardColumn.A == column)
             {
                 A1 = fieldState;
+            }
+
+            if (CheesboardRow.One == row && CheesboardColumn.B == column)
+            {
+                B1 = fieldState;
+            }
+
+            if (CheesboardRow.Two == row && CheesboardColumn.A == column)
+            {
+                A2 = fieldState;
+            }
+
+            if (CheesboardRow.Two == row && CheesboardColumn.B == column)
+            {
+                B2 = fieldState;
             }
 
             if (CheesboardRow.Eight == row && CheesboardColumn.H == column)
