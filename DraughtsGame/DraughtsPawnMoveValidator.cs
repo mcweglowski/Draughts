8a0c4ee2f9b24cb1cc7f2798cfd1972f44383483
Marcin Weglowski <marcin@escali.no>
2019-01-23 09:56:57 +0100

    Fixed bug in DraughtsPawnMoveValidator. Tests are in the same state as before Logic change

--- DraughtsGame/DraughtsPawnMoveValidator.cs
+++ DraughtsGame/DraughtsPawnMoveValidator.cs
@@ -22,7 +22,7 @@ namespace DraughtsGame
 
         public bool IsMoveAvaliable(CheesboardFieldCoordinates sourceField, CheesboardFieldCoordinates destinationField)
         {
-            if (true == IsFieldEmpty(destinationField))
+            if (false == IsFieldEmpty(destinationField))
             {
                 return false;
             }
