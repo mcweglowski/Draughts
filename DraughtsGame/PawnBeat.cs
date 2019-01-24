a489b586db4e4c282470b33569f1dd481e1027a0
Marcin Weglowski <marcin@escali.no>
2019-01-24 11:59:12 +0100

    PawnBeat class, REFACTOR

--- DraughtsGame/PawnBeat.cs
+++ DraughtsGame/PawnBeat.cs
@@ -9,8 +9,7 @@ namespace DraughtsGame
 {
     public class PawnBeat : IPawnMove
     {
-        private ICheesboard cheeseboard;
-
+        ICheesboard cheesboard;
         private PawnBeat()
         {
 
@@ -18,26 +17,67 @@ namespace DraughtsGame
 
         public PawnBeat(ICheesboard cheeseboard)
         {
-            this.cheeseboard = cheeseboard;
+            this.cheesboard = cheeseboard;
         }
 
         public void Move(ICheesboardFieldCoordinates sourceFieldCoordinates, ICheesboardFieldCoordinates destinationFieldCoordinates)
         {
+            IPawn pawn = cheesboard.PickPawn(sourceFieldCoordinates);
+            cheesboard.SetPawn(destinationFieldCoordinates, pawn);
+
             ICheesboardFieldCoordinates middleFieldCoordinates = GetMiddleField(sourceFieldCoordinates, destinationFieldCoordinates);
-            IPawn beaterPawn = cheeseboard.PickPawn(sourceFieldCoordinates);
-            cheeseboard.SetPawn(destinationFieldCoordinates, beaterPawn);
-            cheeseboard.PickPawn(middleFieldCoordinates);
+            cheesboard.PickPawn(middleFieldCoordinates);
         }
 
         private ICheesboardFieldCoordinates GetMiddleField(ICheesboardFieldCoordinates sourceFieldCoordinates, ICheesboardFieldCoordinates destinationFieldCoordinates)
         {
-            IList<CheesboardRow> rows = Enum.GetValues(typeof(CheesboardRow)).Cast<CheesboardRow>().ToList();
-            CheesboardRow cheesboardRow = rows.First(x => (x > sourceFieldCoordinates.Row && x < destinationFieldCoordinates.Row) || (x < sourceFieldCoordinates.Row && x > destinationFieldCoordinates.Row));
+            IList<CheesboardRow> cheeseboardRowsList = GetRowsList();
+            CheesboardRow cheesboardRow = GetMiddleRow(cheeseboardRowsList, sourceFieldCoordinates.Row, destinationFieldCoordinates.Row);
 
-            IList<CheesboardColumn> columns = Enum.GetValues(typeof(CheesboardColumn)).Cast<CheesboardColumn>().ToList();
-            CheesboardColumn cheesboardColumn = columns.First(x => (x > sourceFieldCoordinates.Column && x < destinationFieldCoordinates.Column) || (x < sourceFieldCoordinates.Column && x > destinationFieldCoordinates.Column));
+            IList<CheesboardColumn> cheesboardColumnsList = GetColumnsList();
+            CheesboardColumn cheesboardColumn = GetMiddleColumn(cheesboardColumnsList, sourceFieldCoordinates.Column, destinationFieldCoordinates.Column);
 
             return new CheesboardFieldCoordinates(cheesboardRow, cheesboardColumn);
+        }
+
+        private CheesboardRow GetMiddleRow(IList<CheesboardRow> cheesboardRows, CheesboardRow sourceRow, CheesboardRow destinationRow)
+        {
+            return cheesboardRows.First(x => true == IsRowInRowsRange(x, sourceRow, destinationRow));
+        }
+
+        private CheesboardColumn GetMiddleColumn(IList<CheesboardColumn> cheesboardColumns, CheesboardColumn sourceColumn, CheesboardColumn destinationColumn)
+        {
+            return cheesboardColumns.First(x => true == IsColumnInColumnsRange(x, sourceColumn, destinationColumn));
+        }
+
+        private IList<CheesboardRow> GetRowsList()
+        {
+            return Enum.GetValues(typeof(CheesboardRow)).Cast<CheesboardRow>().ToList();
+        }
+
+        private IList<CheesboardColumn> GetColumnsList()
+        {
+            return Enum.GetValues(typeof(CheesboardColumn)).Cast<CheesboardColumn>().ToList();
+        }
+
+        private bool IsRowInRowsRange(CheesboardRow testedRow, CheesboardRow sourceRow, CheesboardRow destinationRow)
+        {
+            if (testedRow > sourceRow && testedRow < destinationRow)
+            {
+                return true;
+            }
+
+            return testedRow < sourceRow && testedRow > destinationRow;
+        }
+
+        private bool IsColumnInColumnsRange(CheesboardColumn testedColumn, CheesboardColumn sourceColumn, CheesboardColumn destinationColumn)
+        {
+            if (testedColumn > sourceColumn && testedColumn < destinationColumn)
+            {
+                return true;
+            }
+
+            return testedColumn < sourceColumn && testedColumn > destinationColumn;
         }
     }
 }
