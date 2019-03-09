8086ae0eb66e33748baf1b7e2c167a3fe3aca6e5
Marcin Weglowski <marcin@escali.no>
2019-03-09 23:03:07 +0100

    StringToCheesboardFieldConverter test

---
+++ DraughtsGame/StringToCheesboardFieldConverter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraughtsGame
+{
+    public class StringToCheesboardFieldConverter
+    {
+        public CheesboardFieldCoordinates Convert(string a4)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DraughtsGameUnitTests/StringToCheesboardFieldConverterTests.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DraughtsGame.Tests_StringToCheesboardFieldConverterTests
+{
+    [TestClass()]
+    public class StringToCheesboardFieldConverterTests
+    {
+        [TestMethod()]
+        public void shouldConvertStringToCheesboardField()
+        {
+            string A4 = "A4";
+            CheesboardFieldCoordinates expectedCheesboardFieldCoordinates = new CheesboardFieldCoordinates(CheesboardRow.Four, CheesboardColumn.A);
+
+            StringToCheesboardFieldConverter stringToCheesboardFieldConverter = new StringToCheesboardFieldConverter();
+            CheesboardFieldCoordinates actualCheesboardFieldCoordinates = stringToCheesboardFieldConverter.Convert(A4);
+
+            Assert.AreEqual(expectedCheesboardFieldCoordinates, actualCheesboardFieldCoordinates);
+        }
+    }
+}
